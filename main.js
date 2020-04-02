11aefb7db6072433076c1c16445b3d4c8d9216fe
dmitriy365377 <tigr365377@gmail.com>
2020-04-03 01:57:37 +0300

    try to fix fetch

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
 // Эта функция по идее должна быть импортирована,
 // но упрощено и нужно её простейшим образом реализовать
-const serverApiRequest = a => {
+async function serverApiRequest(a) {
   /*simulate request*/
-  return fetch("//t.syshub.ru" + a);
+  const url = "//t.syshub.ru"
+  try {
+    const res = await fetch(url + a);
+    return await res.json()
+  } catch (err) {
+    console.log(
+      "There has been a problem with your fetch operation",
+      err.message
+    )
+  }
 };
 
 // Можно выполнить по аналогии с serverApiRequest(), а можно лучше, см. подсказку ниже
-const sendAnalytics = (a, b) => {
+async function sendAnalytics(a, b) {
   /*sendBeacon maybe*/
+  const url = "//t.syshub.ru"
+  try {
+    const response = await fetch(url + a, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body:  {"data":[b]}
+    });
+    const content = await response.json();
+    return content
+  } catch (err) {
+    alert(
+      "There has been a problem with your fetch operation",
+      err.message
+    )
+  }
 };
 
 /* Нужно:
@@ -16,7 +47,7 @@ const sendAnalytics = (a, b) => {
     3 Подсветить места, где ТЗ недостаточно
     4 Подсветить места, вероятно проблемные
 */
-const requestData = (/*{ id, param }*/) => {
+const requestData = ({ id, param }) => {
   // should return [null, {v: 1}, {v: 4}, null] or Error (may return array (null | {v: number})[])
   var array = serverApiRequest("/query/data/" + id + "/param/" + param);
 
@@ -28,7 +59,7 @@ const requestData = (/*{ id, param }*/) => {
   });
 
   // магия, описать
-  return array2; // return [1, 4]
+  return array; // return [1, 4]
 };
 
 // app proto
@@ -39,10 +70,10 @@ const requestData = (/*{ id, param }*/) => {
     app.appendChild(document.createTextNode(JSON.stringify(text, null, 2)));
     app.appendChild(document.createElement("br"));
   };
-  
+
   log(await requestData({ id: 1, param: "any" }));
   log(await requestData({ id: 4, param: "string" }));
-  log(await requestData({ id: 4, param: 404 }));
+  log(await requestData({ id: 4, param: "any" }));
 })();
 // END DO NOT EDIT app
 
